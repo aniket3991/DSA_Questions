3903d8af0c07d9d3ae300ed30bd507e666b333bd
Aniket Mishra <3991aniketmishra@gmail.com>
2023-03-21 17:07:34 +0530

    Function Completed

--- LeetCode/medium/DivedeTwoInteger.java
+++ LeetCode/medium/DivedeTwoInteger.java
@@ -2,37 +2,33 @@ package medium;
 
 public class DivedeTwoInteger {
     public int divide(int dividend, int divisor) {
-        /*int quetoient = 0;
-        Boolean flag = false;
-        Boolean flag2 = true;
-        if(dividend == Integer.MIN_VALUE) {
-        }
-        if(dividend < 0 && divisor < 0) flag2 = false;
+        int quotient = 0;
 
-        if(dividend < 0) {
-            dividend *= -1;
-            flag = true;
-        }
-        if(divisor < 0) {
-            divisor *= -1;
-            flag = true;
-        }
+        if(dividend == 1 << 31 && divisor == -1) return Integer.MAX_VALUE;
 
-        if (dividend >= divisor) {
-            while (dividend >= divisor) {
-                dividend -= divisor;
-                quetoient++;
+        Boolean sign = (dividend > 0) == (divisor > 0);
+
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+        while (dividend - divisor >= 0) {
+            int count = 0;
+
+            while (dividend - (divisor << 1 << count) >= 0) {
+                count++;
             }
+
+            quotient += 1 << count;
+            dividend -= divisor << count;
         }
 
-        return flag && flag2 ? quetoient * -1 : quetoient;*/
-
-        // Working on this code
-        return 0;
+        return sign ? quotient : -quotient;
     }
 
     public static void main(String[] args) {
+
         DivedeTwoInteger divedeTwoInteger = new DivedeTwoInteger();
-        System.out.println(divedeTwoInteger.divide(1, 1));
+
+        System.out.println(divedeTwoInteger.divide(123, -9));
     }
 }
